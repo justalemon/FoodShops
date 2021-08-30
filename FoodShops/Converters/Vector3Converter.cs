64a318ae60a72f82cb67ba28194dde011efc3a5a
Hannele Ruiz <justlemoncl@gmail.com>
2021-08-30 16:45:30 -0400

    Fixed converter not being able to handle null values (#7)

--- FoodShops/Converters/Vector3Converter.cs
+++ FoodShops/Converters/Vector3Converter.cs
@@ -8,10 +8,15 @@ namespace FoodShops.Converters
     /// <summary>
     /// Converts a Vector3 to JSON and vice versa.
     /// </summary>
-    public class Vector3Converter : JsonConverter<Vector3>
+    public class Vector3Converter : JsonConverter<Vector3?>
     {
-        public override Vector3 ReadJson(JsonReader reader, Type objectType, Vector3 existingValue, bool hasExistingValue, JsonSerializer serializer)
+        public override Vector3? ReadJson(JsonReader reader, Type objectType, Vector3? existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return null;
+            }
+
             JObject @object = JObject.Load(reader);
             float x = @object.ContainsKey("x") ? (float)@object["x"] : 0;
             float y = @object.ContainsKey("y") ? (float)@object["y"] : 0;
@@ -19,13 +24,19 @@ namespace FoodShops.Converters
             return new Vector3(x, y, z);
         }
 
-        public override void WriteJson(JsonWriter writer, Vector3 value, JsonSerializer serializer)
+        public override void WriteJson(JsonWriter writer, Vector3? value, JsonSerializer serializer)
         {
+            if (!value.HasValue)
+            {
+                writer.WriteNull();
+                return;
+            }
+
             JObject @object = new JObject
             {
-                ["x"] = value.X,
-                ["y"] = value.Y,
-                ["z"] = value.Z
+                ["x"] = value.Value.X,
+                ["y"] = value.Value.Y,
+                ["z"] = value.Value.Z
             };
             @object.WriteTo(writer);
         }
